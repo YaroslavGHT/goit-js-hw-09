a1f505b37e03cb63e22491c4d07efd6b139aca12
YaroslavGHT <137333887+YaroslavGHT@users.noreply.github.com>
2023-11-02 20:03:32 +0000

    Deploying to gh-pages from @ YaroslavGHT/goit-js-hw-09@faf67bf7bfe0d92faba1cfd37da83aa128e5361e 🚀

---
+++ 01-color-switcher.e3f58836.js
@@ -0,0 +1,2 @@
+let e=!1;const t=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]"),a=document.querySelector("body");t.disabled=!1,d.disabled=!0,e||(t.addEventListener("click",(()=>{e=!0,t.disabled=!0,d.disabled=!1,changeColor=setInterval((()=>{a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),d.addEventListener("click",(()=>{clearInterval(changeColor),t.disabled=!1,d.disabled=!0})));
+//# sourceMappingURL=01-color-switcher.e3f58836.js.map
