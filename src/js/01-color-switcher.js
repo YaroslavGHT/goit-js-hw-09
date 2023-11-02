475d6ee4f70cf395b435f510415e5fc410a3cfaa
unknown <gtx6600@gmail.com>
2023-11-02 22:13:26 +0200

    two

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,6 +2,7 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
+let changeColor;
 let startActive = false;
 const btnStart = document.querySelector("button[data-start]");
 const btnStop = document.querySelector("button[data-stop]");
